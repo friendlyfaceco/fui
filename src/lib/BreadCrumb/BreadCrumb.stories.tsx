d018ecd2fed8fb6fdc6afff710b3719ddbf69d0c
gcid <gcid12@gmail.com>
2025-01-04 17:03:42 -0800

    Move Breadcrumb to component

--- src/lib/BreadCrumb/BreadCrumb.stories.tsx
+++ src/lib/BreadCrumb/BreadCrumb.stories.tsx
@@ -3,7 +3,7 @@ import type {Meta} from '@storybook/react';
 import {BreadCrumb, Breadcrumbs} from './BreadCrumb';
 
 const meta = {
-    title: 'Example/BreadCrumb',
+    title: 'Component/BreadCrumb',
     component: BreadCrumb,
     parameters: {
         // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
